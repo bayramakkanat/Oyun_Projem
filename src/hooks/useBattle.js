422abde1408ac2da44edc17492224f7b1ff29b46
bayramakkanat <bayramakkanat@gmail.com>
2026-03-12 16:03:53 +0300

    ejderha fix

--- src/hooks/useBattle.js
+++ src/hooks/useBattle.js
@@ -1092,7 +1092,13 @@ triggerAnim(snipeTarget.id, "damage");
           setET([...ee]);
           if (isCancelled) return;
         }
-
+// Oyuncu ejderhası ATK buff
+pp.forEach((a, i) => {
+  const m = pwr(a);
+  if (a.ability === "start_fire") {
+    pp[i].atk = clampStat(pp[i].atk + 4 * m);
+  }
+});
         // Düşman buff yetenekleri (start_fire ile ATK buff alan ejderha için)
         ee.forEach((a, i) => {
           const m = pwr(a);
